8bc6236b7ec19b25f7c89d47400685f0fffc397d
adrian03660 <adjodero81@gmail.com>
2020-09-30 07:58:54 -0500

    Update Baloto.java

--- Fuente/baloto/Baloto.java
+++ Fuente/baloto/Baloto.java
@@ -15,8 +15,7 @@ public class Baloto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo");
-	        //Para rifar el numero
+	//Para rifar el numero
         Random baloto = new Random();
         int [] n = new int [6];
 	int [] nBaloto = new int [6];
