ce6fabb800914c0a03de939f5c2e6d2c9b975cb0
adrian03660 <adjodero81@gmail.com>
2020-10-03 22:43:48 -0500

    Update Baloto.java

--- Fuente/baloto/Baloto.java
+++ Fuente/baloto/Baloto.java
@@ -52,5 +52,6 @@ public class Baloto {
 	    System.err.println("Siga intentando, no acertó en ninguno de los numero");
     }
             System.out.println("El numero que usted digitó fue:"+nIngresado[0]+nIngresado[1]+nIngresado[2]+nIngresado[3]+nIngresado[4]+nIngresado[5]);
+            System.out.println("El numero que jugó fue:"+nBaloto[5]+nBaloto[4]+nBaloto[3]+nBaloto[2]+nBaloto[1]+nBaloto[0]);
 
 }
