7f274032c6306390bc68e37bdb794af5e8c51a27
Sergio1-Git <mercadosergio754@gmail.com>
2020-10-04 11:59:06 -0500

    background update

---
+++ Fuente/baloto/Baloto.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baloto;
+
+/**
+ *
+ * @author usuario
+ */
+public class Baloto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	//Para rifar el numero
+        Random baloto = new Random();
+        int [] n = new int [6];
+	int [] nBaloto = new int [6];
+	int [] nIngresado = new int [6];
+	int numrifado;
+	
+    }
+    
+}
