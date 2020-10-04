12c8abcad37470e766a5a8bebefd2e4082d81abf
adrian03660 <adjodero81@gmail.com>
2020-10-03 22:25:34 -0500

    Update Baloto.java

--- Fuente/baloto/Baloto.java
+++ Fuente/baloto/Baloto.java
@@ -42,8 +42,9 @@ public class Baloto {
 	    System.out.println("Usted acertó en tres numeros");	
 	   //Si acierta en 5 numeros 
 	}else if (nIngresado[5] == nBaloto[0] && nIngresado [4] == nBaloto[1] && nIngresado[3] == nBaloto[2] && nIngresado[2] == nBaloto[3] && nIngresado[1] == nBaloto[4]){
-	    System.out.println("¡Felicidades!, Usted se ganó:"+pago3);
-  	    System.out.println("Usted acertó en todos los numeros");
+	    System.out.println("¡Felicidades!, Usted se ganó:"+pago2);
+  	    System.out.println("Usted acertó en cinco numeros");
+
 
 	}
     }
